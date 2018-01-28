9996bfb69375877249864304feb6c8aa88a3b649
Taehee Kim <slhyvaa@nate.com>
2018-01-28 14:50:56 +0900

    Migrate hapi version from 16 to 17

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,3 @@
 import * as Server from "./server";
 
 const server = Server.init();
-
-server.start((err: Error) => {
-  if (err) {
-    throw err;
-  }
-  console.log(`Server running at : ${server.info.uri}`);
-});
--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,24 @@
 import * as Hapi from "hapi"
 
-export function init(): Hapi.Server {
+export async function init() {
 
-  const server: Hapi.Server = new Hapi.Server();
-  const connectionSettings = {
+  const server: any = new Hapi.Server({
     port: 3000,
     host: 'localhost'
-  };
-
-  server.connection(connectionSettings);
+  });
 
   server.route({
     method: 'GET',
     path: '/getResult',
-    handler: function (request: Hapi.Request, reply: Hapi.ReplyNoContinue) {
-      reply(`Hello`);
+    handler: function (request: any) {
+      return 'Hello';
     }
   })
 
-  return server;
+  try {
+    await server.start();
+    console.log(`Server running at : ${server.info.uri}`);
+  } catch (err) {
+    console.log(`Server err : ${err}`);
+  }
 }
